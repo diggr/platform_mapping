f93ec8406e235e1e5a94da664eb24d102408ef3d
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2018-03-06 18:01:33 +0100

    new export formats + templated index page

--- scripts/convert_tsv_to_json.py
+++ scripts/convert_tsv_to_json.py
@@ -1,20 +1,45 @@
 #!/usr/bin/env python3
 """
-Converts platform mapping tsv and csv files to json files.
+Converts platform mapping tsv files to json, xlsx and csv files, to be used as a static rest api.
+
+Copyright (C) 2018 Leipzig University Library <info@ub.uni-leipzig.de>
+
+@author   Florian Rämisch <raemisch@ub.uni-leipzig.de>
+@license  https://opensource.org/licenses/GPL-3.0 GNU GPLv3
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2,
+as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 """
 import csv
 import datetime
 import json
+import io
+import shutil
+import markdown
 
-from os.path import join, abspath, dirname
-
+from os.path import join, abspath, dirname, isdir
+from os import mkdir, listdir
+from openpyxl import Workbook
+from jinja2 import Template
 
 # CONFIGURATION
 
 ## DELIMITER OF THE BASE DATA
 DELIMITER="\t"
 
-## BASE PATH IS PREPENDED TO ALL OTHER DATA PATHS 
+## BASE PATH IS PREPENDED TO ALL OTHER DATA PATHS
 BASE_PATH = join(dirname(abspath(__file__)), "..")
 
 ## SKELETON IS LOCATED AT
@@ -35,12 +60,20 @@ MAPPING_FILES = { m_title:join(MAPPING_PATH, m_filename) for m_title, m_filename
 ## PLATFORM GROUP FILE
 PLATFORM_GROUP_FILE = join(MAPPING_PATH, "platform_groups.tsv")
 
-## OUTPUT 
-OUT_PATH = join(BASE_PATH, "json_data")
+## OUTPUT
+OUT_PATH = join(BASE_PATH, "static_rest_api")
 OUT_EXT = ".json"
 PLATFORM_GROUP_OUTFILE = join(OUT_PATH, "platform_groups"+OUT_EXT)
 
-# FUNCTIONS
+## INDEX PAGE
+INDEX_TEMPLATE = join(BASE_PATH, "templates", "index.html")
+README = join(BASE_PATH, "README.md")
+INDEX_PAGE = join(OUT_PATH, "index.html")
+
+if not isdir(OUT_PATH):
+    mkdir(OUT_PATH)
+
+# MAPPING FUNCTIONS
 def mapping_to_json(mapping_file, delimiter=DELIMITER):
     """
     Opens a tabular (default) or any other kind of two-column csv-data
@@ -49,18 +82,52 @@ def mapping_to_json(mapping_file, delimiter=DELIMITER):
     output = dict()
     with open(mapping_file) as mfile:
         reader = csv.reader(mfile, delimiter=delimiter)
-        
+
         # skip header
         next(reader)
-        
+
         for row in reader:
             output[row[0]] = row[1]
     return output
 
+
+def mapping_to_xlsx(mapping_file, delimiter=DELIMITER):
+    """
+    Opens a tabular (default) or any other kind of two-column csv-data
+    and returns its content as python object
+    """
+    with open(mapping_file) as mfile:
+        reader = csv.reader(mfile, delimiter=delimiter)
+
+        wb = Workbook()
+        ws = wb.active
+
+        for r, row in enumerate(reader,1):
+            ws.cell(row=r, column=1, value=row[0])
+            ws.cell(row=r, column=2, value=row[1])
+
+    return wb
+
+
+def mapping_to_csv(mapping_file, delimiter=DELIMITER):
+    """
+    Opens a tabular (default) or any other kind of two-column csv-data
+    and returns its content as python object
+    """
+    with open(mapping_file) as mfile:
+        reader = csv.reader(mfile, delimiter=delimiter)
+        outfile = io.StringIO()
+        output = csv.writer(outfile, delimiter=";", quotechar='"')
+
+        for row in enumerate(reader):
+            output.writerow(row[1])
+
+        return outfile
+
 def platform_group_to_json(platform_group_file, delimiter=DELIMITER):
     with open(platform_group_file) as pffile:
         reader = csv.reader(pffile, delimiter=delimiter)
-        
+
         result = {}
         groupname = ""
         group = list()
@@ -71,8 +138,33 @@ def platform_group_to_json(platform_group_file, delimiter=DELIMITER):
                 result[row[0]].append(platform_name)
             except KeyError:
                 result[row[0]] = [platform_name]
-           
-        return result 
+
+        return result
+
+def platform_group_to_xlsx(platform_group_file, delimiter=DELIMITER):
+    with open(platform_group_file) as pffile:
+        reader = csv.reader(pffile, delimiter=delimiter)
+
+        wb = Workbook()
+        ws = wb.active
+
+        for r, row in enumerate(reader):
+            ws.cell(row=1, column=0, value=row[0])
+            ws.cell(row=1, column=1, value=row[1])
+
+        return wb
+
+def platform_group_to_csv(platform_group_file, delimiter=DELIMITER):
+    with open(platform_group_file) as pffile:
+        reader = csv.reader(pffile, delimiter=delimiter)
+
+        outfile = io.StringIO()
+        output = csv.writer(outfile, delimiter=";", quotechar='"')
+
+        for r, row in enumerate(reader):
+            output.writerow(row[1])
+
+        return outfile
 
 def load_skeleton(skeleton_filename=SKELETON_FILENAME):
     """
@@ -82,8 +174,23 @@ def load_skeleton(skeleton_filename=SKELETON_FILENAME):
     with open(skeleton_filename) as sfile:
         return json.load(sfile)
 
+def make_index_page(index_template=INDEX_TEMPLATE, readme=README, index_page=INDEX_PAGE, out_path=OUT_PATH):
+    files = listdir(out_path)
+    files.sort()
+    for filename in files:
+        ext = filename.split(".")[-1]
+        print(filename, ext)
+        if ext not in ["csv", "xlsx", "json"]:
+            files.remove(filename)
+    with open(readme) as readme_file:
+        readme_text = readme_file.read()
+    readme_html = markdown.markdown(readme_text)
+    with open(index_template) as index:
+        template = Template(index.read())
+    with open(index_page, "w") as index_page_file:
+        index_page_file.write(template.render(README=readme_html, files=files))
 
-def create_static_rest_files(mapping_files=MAPPING_FILES, 
+def create_static_rest_files(mapping_files=MAPPING_FILES,
                              platform_group_file=PLATFORM_GROUP_FILE,
                              platform_group_outfile=PLATFORM_GROUP_OUTFILE,
                              delimiter=DELIMITER,
@@ -96,21 +203,33 @@ def create_static_rest_files(mapping_files=MAPPING_FILES,
     """
     # Gather
     skeleton = load_skeleton(skeleton_filename)
-    skeleton["date"] = datetime.datetime.now().replace(microsecond=0).isoformat()
+    skeleton["meta"]["date"] = datetime.datetime.now().replace(microsecond=0).isoformat()
 
     for m_title, m_filename in mapping_files.items():
         print("Converting {} ...".format(m_title))
-        mapping = mapping_to_json(m_filename)
-        
+        json_mapping = mapping_to_json(m_filename)
+        csv_mapping = mapping_to_csv(m_filename)
+        xlsx_mapping = mapping_to_xlsx(m_filename)
+
         # Combine
         out_data = skeleton.copy()
-        out_data["name"] = m_title
-        out_data["mapping"] = mapping
+        out_data["meta"]["name"] = m_title
+        out_data["mapping"] = json_mapping
 
-        # Save
-        out_file = join(out_path, m_title.lower().replace(" ", "_")+out_ext)
-        with open(out_file, "w") as outfile:
+        # Save JSON
+        json_out_file = join(out_path, m_title.lower().replace(" ", "_")+out_ext)
+        with open(json_out_file, "w") as outfile:
             json.dump(out_data, outfile, indent=4)
+
+        # Save CSV
+        csv_out_file = join(out_path, m_title.lower().replace(" ", "_")+".csv")
+        with open(csv_out_file, "w") as outfile:
+            csv_mapping.seek(0)
+            shutil.copyfileobj(csv_mapping, outfile)
+
+        # Save XLSX
+        xlsx_out_file = join(out_path, m_title.lower().replace(" ", "_")+".xlsx")
+        xlsx_mapping.save(xlsx_out_file)
 
     #Platform Group
     print("Converting Platform Groups ...")
@@ -123,3 +242,4 @@ def create_static_rest_files(mapping_files=MAPPING_FILES,
 
 if __name__ == "__main__":
     create_static_rest_files()
+    make_index_page()
